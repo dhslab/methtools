975e79173d5043f192230a31723a46d3c8312df7
Mohamed Mahgoub <mmohamed@mohameds-macbook-pro-3.local.dhcp.wustl.edu>
2025-05-28 20:21:33 -0500

    first commit

--- methtools/utils/meth_stats.py
+++ methtools/utils/meth_stats.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 
 def fisher_exact_test(
-    sample1_meth: int, sample1_unmeth: int, sample2_meth: int, sample2_unmeth: int
+    group1_meth: int, group1_unmeth: int, group2_meth: int, group2_unmeth: int
 ):
     """
-    Perform Fisher's Exact Test to compare methylation counts between two samples.
+    Perform Fisher's Exact Test to compare methylation counts between two groups.
 
     Parameters:
-    sample1_meth (int): Methylated CpG count for sample 1.
-    sample1_unmeth (int): Unmethylated CpG count for sample 1.
-    sample2_meth (int): Methylated CpG count for sample 2.
-    sample2_unmeth (int): Unmethylated CpG count for sample 2.
+    group1_meth (int): Methylated CpG count for group 1.
+    group1_unmeth (int): Unmethylated CpG count for group 1.
+    group2_meth (int): Methylated CpG count for group 2.
+    group2_unmeth (int): Unmethylated CpG count for group 2.
 
     Returns:
     dict: A dictionary containing:
@@ -20,8 +20,8 @@ def fisher_exact_test(
         - 'fisher_p_value': p-value from Fisher's Exact Test
     """
     table = [
-        [sample1_meth, sample1_unmeth],
-        [sample2_meth, sample2_unmeth],
+        [group1_meth, group1_unmeth],
+        [group2_meth, group2_unmeth],
     ]
 
     odds_ratio, p_value = stats.fisher_exact(table, alternative="two-sided")
